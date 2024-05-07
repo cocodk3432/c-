b23a7830870cbfe7f9900ba7c9be9f298cedca24
Dhiraj <dhirajkuttikar@gmail.com>
2024-05-07 15:43:31 +0530

    Update triangle _triplet.cpp

--- triangle _triplet.cpp
+++ triangle _triplet.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
+bool triangel_triplet(int a,int b, int c){
+  bool flag;
+  max = max(a,max(b,c));
+  if (a == max)
+  {
+    
+  }
+  return 0;
+}
+int main(){
 
+
+return 0;
+}
